7012fb5bb151274152b43bc9d56da31376f27039
Alireza6677 <alireza6677@gmail.com>
2019-09-29 17:23:25 +0330

    First commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Bluetooth Headset Battery Level
+
+This is a simple python script to print the battery charge level of some bluetooth headsets
+
+**Notice: You should disconnect the headset before running the script**
+
+# How to run
+First, change the `BT_ADDRESS` variable to your device's address, Then run the script
+```bash
+python3 ./bl_battery.py
+```
+
+# License
+GPL-3.0
+
---
+++ bl_battery.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+#
+# This script prints the battery charge level of some bluetooth headsets
+# Notice: Headset should be disconnected before running this script
+# Define your BT_ADDRESS before using
+# 
+
+# License: GPL-3.0
+# Author: @TheWeirdDev
+# 29 Sept 2019
+
+import socket
+
+# Change this
+BT_ADDRESS = "XX:XX:XX:XX:XX:XX"
+
+def send(sock, message):
+	sock.send(b"\r\n" + message + b"\r\n")
+
+def getATCommand(sock, line):
+	if b"BRSF" in line:
+		send(sock, b"+BRSF:20")
+		send(sock, b"OK")
+	elif b"CIND=" in line:
+		send(sock, b"+CIND: (\"battchg\",(0-5))")
+		send(sock, b"OK")
+	elif b"CIND?" in line:
+		send(sock, b"+CIND: 5")
+		send(sock, b"OK")
+	elif b"IPHONEACCEV" in line:
+		parts = line.split(b",")
+		
+		# This might print the wrong value if dock state is received
+		blevel = (int(parts[-1]) + 1) * 10
+		print("Battery level: {}%\n".format(blevel))
+		return False
+	else:
+		send(sock, b"OK")
+	return True
+
+def main():
+	if (BT_ADDRESS.startswith("XX")):
+		print("You need to define bluetooth adress of your device first\n")
+		print("BT_ADDRESS =", BT_ADDRESS)
+		exit()
+		
+	s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+	try:
+		s.connect((BT_ADDRESS, 4))
+		
+		while getATCommand(s, s.recv(128)):
+			pass
+
+	finally:
+		s.shutdown(1)
+		
+if __name__ == "__main__":
+    main()
+
+		
+		
+

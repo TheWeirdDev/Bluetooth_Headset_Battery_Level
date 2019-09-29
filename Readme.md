b0cb4f4c6af38b9fae36f508f798ae97eeda6278
Alireza6677 <alireza6677@gmail.com>
2019-09-29 19:26:45 +0330

    No need to disconnect the headset anymore

--- Readme.md
+++ Readme.md
@@ -2,8 +2,6 @@
 
 This is a simple python script to print the battery charge level of some bluetooth headsets
 
-**Notice: You should disconnect the headset before running the script**
-
 # How to run
 First, change the `BT_ADDRESS` variable to your device's address, Then run the script
 ```bash
--- bl_battery.py
+++ bl_battery.py
@@ -13,48 +13,48 @@
 import socket
 
 # Change this
-BT_ADDRESS = 'FC:03:F6:DC:52:52'
+BT_ADDRESS = 'XX:XX:XX:XX:XX:XX'
 
 def send(sock, message):
-	sock.send(b"\r\n" + message + b"\r\n")
+    sock.send(b"\r\n" + message + b"\r\n")
 
 def getATCommand(sock, line):
-	if b"BRSF" in line:
-		send(sock, b"+BRSF:20")
-		send(sock, b"OK")
-	elif b"CIND=" in line:
-		send(sock, b"+CIND: (\"battchg\",(0-5))")
-		send(sock, b"OK")
-	elif b"CIND?" in line:
-		send(sock, b"+CIND: 5")
-		send(sock, b"OK")
-	elif b"IPHONEACCEV" in line:
-		parts = line.split(b",")
-		
-		# This might print the wrong value if dock state is received
-		blevel = (int(parts[-1]) + 1) * 10
-		print("Battery level: {}%\n".format(blevel))
-		return False
-	else:
-		send(sock, b"OK")
-	return True
+    if b"BRSF" in line:
+        send(sock, b"+BRSF:20")
+        send(sock, b"OK")
+    elif b"CIND=" in line:
+        send(sock, b"+CIND: (\"battchg\",(0-5))")
+        send(sock, b"OK")
+    elif b"CIND?" in line:
+        send(sock, b"+CIND: 5")
+        send(sock, b"OK")
+    elif b"IPHONEACCEV" in line:
+        parts = line.split(b",")
+
+        # This might print the wrong value if dock state is received
+        blevel = (int(parts[-1]) + 1) * 10
+        print("Battery level: {}%\n".format(blevel))
+        return False
+    else:
+        send(sock, b"OK")
+    return True
 
 def main():
-	if (BT_ADDRESS.startswith("XX")):
-		print("You need to define bluetooth adress of your device first\n")
-		print("BT_ADDRESS =", BT_ADDRESS)
-		exit()
+    if (BT_ADDRESS.startswith("XX")):
+        print("You need to define bluetooth adress of your device first\n")
+        print("BT_ADDRESS =", BT_ADDRESS)
+        exit()
 		
-	s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-	try:
-		s.connect((BT_ADDRESS, 4))
-		
-		while getATCommand(s, s.recv(128)):
-			pass
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s.connect((BT_ADDRESS, 4))
 
-	finally:
-		s.shutdown(socket.SHUT_RDWR)
-		
+    while getATCommand(s, s.recv(128)):
+        pass
+
+                
 if __name__ == "__main__":
-    main()
+	main()
+
+		
+		
 

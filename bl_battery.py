5a899b4af56f52326c69fdad54d80e01fa2a9104
Alireza6677 <alireza6677@gmail.com>
2019-10-01 22:08:11 +0330

    Fixed parsing the IPHONEACCEV command. Fixes #2

--- bl_battery.py
+++ bl_battery.py
@@ -24,12 +24,23 @@ def getATCommand(sock, line):
         send(sock, b"+CIND: 5")
         send(sock, b"OK")
     elif b"IPHONEACCEV" in line:
-        parts = line.split(b",")
-
-        # This might print the wrong value if dock state is received
-        blevel = (int(parts[-1]) + 1) * 10
-        print("Battery level: {}%\n".format(blevel))
-        return False
+        if not b',' in line:
+            return True
+        
+        parts = line[line.index(b',') + 1 : -1].split(b',')
+        if len(parts) < 1 or (len(parts) % 2) != 0:
+            return True
+        
+        i = 0
+        while i < len(parts):
+            key = int(parts[i])
+            val = int(parts[i + 1])
+            
+            if key == 1:
+                blevel = (val + 1) * 10
+                print("Battery level: {}%\n".format(blevel))
+                return False
+            i += 2
     else:
         send(sock, b"OK")
     return True
@@ -40,17 +51,12 @@ def main():
         exit()
     
     BT_ADDRESS = sys.argv[1]
-		
+    
     s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
     s.connect((BT_ADDRESS, 4))
 
     while getATCommand(s, s.recv(128)):
         pass
-
-                
+             
 if __name__ == "__main__":
-	main()
-
-		
-		
-
+    main()
